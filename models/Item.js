5841522d1cf64a041cb177af58c980dbdb2ebf80
Febrianto <febrianto.bekasi@gmail.com>
2022-09-25 06:31:10 +0700

    add schemas

---
+++ models/Item.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const itemSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  country: {
+    type: String,
+    default: "Indonesia",
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  isPopular: {
+    type: Boolean,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageIds: [
+    {
+      type: ObjectId,
+      ref: "Image",
+    },
+  ],
+  featureIds: [
+    {
+      type: ObjectId,
+      ref: "Feature",
+    },
+  ],
+  activityIds: [
+    {
+      type: ObjectId,
+      ref: "Activity",
+    },
+  ],
+});
+
+module.exports = mongoose.model("Item", itemSchema);
